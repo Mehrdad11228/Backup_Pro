c687d7870bf49f6205176da81d8585c06f2bd8a0
Mehrdad11228 <110027923+Mehrdad11228@users.noreply.github.com>
2025-11-05 17:17:47 +0330

    Update backuper_marzneshin.sh

--- backuper_marzneshin.sh
+++ backuper_marzneshin.sh
@@ -63,7 +63,7 @@ rm -rf etc_opt var_lib_marznode var_lib_marzneshin marzneshin_backup.sql
 # Copy paths
 mkdir -p etc_opt var_lib_marznode var_lib_marzneshin
 cp -r /etc/opt/marzneshin/ etc_opt/ 2>/dev/null || true
-cp -r /var/lib/marznode/ var_lib_marznode/ 2>/dev/null || true
+rsync -a --include='xray_config.json' --exclude='*' /var/lib/marznode/ var_lib_marznode/ 2>/dev/null || true
 rsync -a --exclude='mysql' /var/lib/marzneshin/ var_lib_marzneshin/ 2>/dev/null || true
 
 # ==============================
